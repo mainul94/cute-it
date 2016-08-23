c93580069cffb5ee8076739ecc4c9ba40f0cbcbe
Mainul Islam <mainulkhan94@gmail.com>
2016-08-23 14:15:09 +0600

    First Commit Install SOme packgest

---
+++ app/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Bican\Roles\Traits\HasRoleAndPermission;
+use Bican\Roles\Contracts\HasRoleAndPermission as HasRoleAndPermissionContract;
+
+class User extends Authenticatable implements HasRoleAndPermissionContract
+{
+
+    use CanResetPassword, HasRoleAndPermission;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
